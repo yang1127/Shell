13a475f8684681ce3b9f3453befe19b8092aa6d9
yang761127 <1536520643@qq.com>
2021-10-20 14:26:10 +0800

    Shell练习

--- test/test1.sh
+++ test/test1.sh
@@ -21,3 +21,14 @@ geet1='hello, '$name1''
 # 双引号拼接
 geet2="hello, "$name1""
 echo $geet1 $geet2
+
+# 获取字符串长度
+echo ${#geet2}
+
+# 提取字符串
+echo ${string:1:4}
+
+# 查找子字符串
+string="runoob is a great site"
+# 输出4
+echo `expr index "$string" io`
